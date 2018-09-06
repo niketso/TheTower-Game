950d00520b7885b64c777349ab5c6b84f6d64253
Unknown <agustingarcia1997@gmail.com>
2018-09-06 14:59:33 -0300

    Proto-v0.2
    - Matrix de colisiones 2D editada para funcionamiento de colisiones
    - Script MeleeBehaviour añadido
    - PlaceHolder de Melee añadido
    - Deshabilitados los packages de Unity Innecesarios

---
+++ TheTower/Assets/Scripts/Enemy/MeleeBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeBehaviour : MonoBehaviour {
+
+    private GameObject player;
+    private SpriteRenderer sprite;
+
+    private void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update() {
+        if(player.transform.position.x > transform.position.x)
+            GoRight();
+        if(player.transform.position.x < transform.position.x)
+            GoLeft();
+    }
+
+    private void GoLeft() {
+        transform.Translate(-0.02f, 0f, 0f);
+        sprite.flipX = false;
+    }
+
+    private void GoRight() {
+        transform.Translate(0.02f, 0f, 0f);
+        sprite.flipX = true;
+    }
+}
